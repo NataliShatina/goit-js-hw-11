31c8b18863626c00d92f0da3847d923efe98ba98
Natali Shatina <natali.shatina@gmail.com>
2026-06-13 20:37:09 +0200

    main work

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '56296156-8452051e0cb934f68c3ddeebb';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export function getImagesByQuery(query) {
+  return axios.get(BASE_URL, {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    },
+  });
+}
---
+++ src/main.js
@@ -0,0 +1,41 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import { getImagesByQuery } from './js/pixabay-api.js';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions.js';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const query = e.target['search-text'].value.trim();
+
+  clearGallery();
+  showLoader();
+
+  getImagesByQuery(query)
+    .then(response => {
+      if (response.data.hits.length === 0) {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+        return;
+      }
+      createGallery(response.data.hits);
+    })
+    .catch(error => {
+      iziToast.error({
+        message: error.message,
+        position: 'topRight',
+      });
+    })
+    .finally(() => {
+      hideLoader();
+    });
+});
